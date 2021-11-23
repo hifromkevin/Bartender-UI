3b9f36e022cd6bf80e246317e16a75d78e896b8b
hifromkevin <lam.kevin86@gmail.com>
2021-11-23 00:09:28 -0800

    Revises styling

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 const Homepage = (props) => {
   const { channels, cocktails } = props;
+
   return (
     <div className="homepage">
       <h1 className="homepage__logo">Bartender</h1>
@@ -17,41 +18,67 @@ const Homepage = (props) => {
               <div className="mixer">
                 <img
                   className="mixer__image"
-                  src={channel.selectedMixerImage}
+                  src={channel.selectedMixerImage || 'assets/bottle.jpg'}
                 />
-                {(channel.selectedMixerType || channel.selectedMixer) && <div className="mixer__tag">
-                  <span className="mixer__tag__type">{channel.selectedMixerType}</span>
-                  <span className="mixer__tag__brand">{channel.selectedMixer}</span>
-                </div>}
+                <div className="mixer__tag">
+                  <span className="mixer__tag__type">{channel.selectedMixerType || 'No Selection'}</span>
+                  <span className="mixer__tag__brand">{channel.selectedMixer || 'Select Mixer'}</span>
+                </div>
               </div>
-              <p>{channel.channelName}</p>
+              <span className="station__name">{channel.channelName}</span>
             </div>
           ))}
         </div>
       </div>
 
+      {/* <div className="cocktailStation">
+        <h1 className="homepage__heading">Available Cocktails</h1>
+        <div className="cocktailStation__container">
+          <div className="cocktailStation__list" style={{ width: (cocktails.length * 220) }}>
+            {cocktails.map((cocktail, cocktailIndex) => (
+              <div
+                className="cocktail2"
+                key={cocktailIndex}
+              >
+                <img
+                  className="cocktail2__image"
+                  src={cocktail.cocktailImage}
+                />
+                <div className="cocktail2__tag">
+                  <span className="cocktail2__tag__name">{cocktail.cocktailName}</span>
+                  <span className="cocktail2__tag__description">{cocktail.cocktailDescription}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div> */}
+
       <div className="cocktailStation">
         <h1 className="homepage__heading">Available Cocktails</h1>
         <div className="cocktailStation__container">
-          <div className="cocktailStation__list" style={{ width: (cocktails.length * 350) }}>
+          <div className="cocktailStation__list" style={{ width: (cocktails.length * 510) }}>
             {cocktails.map((cocktail, cocktailIndex) => (
               <div
                 className="cocktail"
                 key={cocktailIndex}
               >
-                <img
-                  className="cocktail__image"
-                  src={cocktail.cocktailImage}
-                />
-                <div className="cocktail__tag">
-                  <span className="cocktail__tag__name">{cocktail.cocktailName}</span>
+                <div className="cocktail__container">
+                  <div className="cocktail__container__image">
+                    <img src={cocktail.cocktailImage} />
+                  </div>
+                  <div className="cocktail__container__tag">
+                    <span className="cocktail__container__tag__name">{cocktail.cocktailName}</span>
+                    <hr className="cocktail__container__tag__separator" />
+                    <span className="cocktail__container__tag__description">{cocktail.cocktailDescription}</span>
+                  </div>
                 </div>
               </div>
             ))}
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
