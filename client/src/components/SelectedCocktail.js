80f9738388cef1dd9e8ca79891992ae32a793c91
hifromkevin <lam.kevin86@gmail.com>
2021-11-23 00:07:30 -0800

    Begins design process

--- client/src/components/SelectedCocktail.js
+++ client/src/components/SelectedCocktail.js
@@ -1,8 +1,42 @@
 import 'babel-polyfill';
 import React, { useState, useEffect } from 'react';
 
-const SelectedCocktail = () => {
-  return ('I am cocktail');
+const SelectedCocktail = (props) => {
+  const {
+    cocktail: {
+      cocktailName = '',
+      cocktailDescription = '',
+      cocktailIngredients = '',
+      cocktailImage = ''
+    }
+  } = props;
+
+  return (
+    <div className="selectedCocktail">
+      <div className="selectedCocktail__display"><img className="selectedCocktail__display__image" src={cocktailImage} /></div>
+      <div className="selectedCocktail__content">
+        <h2 className="selectedCocktail__content__name">{cocktailName}</h2>
+        <div className="selectedCocktail__content__ingredients">{
+          cocktailIngredients.map((ingredient, ingredientIndex) => (
+            <div
+              key={ingredientIndex}
+              className="selectedCocktail__content__ingredients__single"
+            >
+              {`${ingredient.ingredientAmount} ${ingredient.ingredientName}`}
+            </div>)
+          )
+        }</div>
+        <div className="selectedCocktail__content__description">{cocktailDescription}</div>
+        <div className="selectedCocktail__content__garnish">
+          <h2>Garnish</h2>
+        </div>
+        <div className="selectedCocktail__content__options">
+          <button>Bottoms Up</button>
+          <a>Go Back</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SelectedCocktail;
