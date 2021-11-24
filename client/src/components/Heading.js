520f7dc3380533dc964b7656b5183c3e95cbe95a
hifromkevin <lam.kevin86@gmail.com>
2021-11-24 01:32:33 -0800

    Adds option to change color

--- client/src/components/Heading.js
+++ client/src/components/Heading.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 
 const Heading = (props) => (
-  <div className="heading">
+  <div className="heading" style={{ 'color': `${props.coloring}` }}>
     <div className="heading__block"></div>
     <h1 className="heading__text">{props.heading}</h1>
   </div>
