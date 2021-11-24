828f9502ace66308b5abb0fa430e40942462a2f6
hifromkevin <lam.kevin86@gmail.com>
2021-11-24 01:34:52 -0800

    Heavily restyles SelectedCocktail component

--- client/src/components/SelectedCocktail.js
+++ client/src/components/SelectedCocktail.js
@@ -1,38 +1,63 @@
 import 'babel-polyfill';
 import React, { useState, useEffect } from 'react';
 
+import Heading from './Heading';
+
 const SelectedCocktail = (props) => {
   const {
     cocktail: {
-      cocktailName = '',
-      cocktailDescription = '',
-      cocktailIngredients = '',
-      cocktailImage = ''
-    }
+      cocktailName,
+      cocktailDescription,
+      cocktailImage,
+      cocktailIngredients,
+      cocktailSteps
+    },
+    setSelectedPage
   } = props;
+
+  const styling = {
+    'backgroundImage': `url(${cocktailImage})`,
+    'backgroundPosition': 'center',
+    'backgroundSize': 'cover'
+  }
+
+  const fancyNumber = (num, instruction) => {
+    if (num < 10) num = `0${num}`;
+
+    return <div className="selectedCocktail__content__step"><span className="selectedCocktail__content__step__number">{num}</span> <span>{instruction}</span></div>
+  }
 
   return (
     <div className="selectedCocktail">
-      <div className="selectedCocktail__display"><img className="selectedCocktail__display__image" src={cocktailImage} /></div>
+      <div className="selectedCocktail__display" style={styling}>
+        {/* <img className="selectedCocktail__display__image" src={cocktailImage} /> */}
+      </div>
       <div className="selectedCocktail__content">
-        <h2 className="selectedCocktail__content__name">{cocktailName}</h2>
-        <div className="selectedCocktail__content__ingredients">{
-          cocktailIngredients.map((ingredient, ingredientIndex) => (
-            <div
-              key={ingredientIndex}
-              className="selectedCocktail__content__ingredients__single"
-            >
-              {`${ingredient.ingredientAmount} ${ingredient.ingredientName}`}
-            </div>)
-          )
-        }</div>
+        <Heading coloring='black' heading={cocktailName} />
+
+        <div className="selectedCocktail__content__ingredients">
+          {
+            cocktailIngredients && cocktailIngredients.map((ingredient, ingredientIndex) => (
+              <div key={ingredientIndex}>
+                {`${ingredient.ingredientAmount} ${ingredient.ingredientName}`}
+              </div>)
+            )
+          }
+        </div>
         <div className="selectedCocktail__content__description">{cocktailDescription}</div>
-        <div className="selectedCocktail__content__garnish">
+        {/* <div className="selectedCocktail__content__garnish">
           <h2>Garnish</h2>
+        </div> */}
+        <div className="selectedCocktail__content__steps">
+          {
+            cocktailSteps && cocktailSteps.map((step, stepIndex) => (
+              fancyNumber(stepIndex + 1, step)
+            ))
+          }
         </div>
         <div className="selectedCocktail__content__options">
-          <button>Bottoms Up</button>
-          <a>Go Back</a>
+          <span className="selectedCocktail__content__options__bottomsUp">Bottoms Up</span>
+          <a onClick={() => setSelectedPage('homepage')}>Go Back</a>
         </div>
       </div>
     </div>
