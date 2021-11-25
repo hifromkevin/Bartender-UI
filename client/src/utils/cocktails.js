45abf6ae93da5e2020d6edcbfdd74fe37e46adde
hifromkevin <lam.kevin86@gmail.com>
2021-11-24 18:59:56 -0800

    Reassigns cocktails and mixers to their own files

---
+++ client/src/utils/cocktails.js
@@ -0,0 +1,115 @@
+const cocktails = [
+  {
+    cocktailName: 'Fancy Name',
+    cocktailDescription: 'Leverage agile frame works to provide a robust synopsis for high level overviews.',
+    cocktailGarnishes: [
+      {
+        garnishName: 'Cocktail Glass',
+        garnishImage: 'assets/images/icons/cocktail.png',
+      },
+      {
+        garnishName: 'Orange Wedge',
+        garnishImage: 'assets/images/icons/wedge.png',
+      },
+      {
+        garnishName: 'Ice Cubes',
+        garnishImage: 'assets/images/icons/ice.png',
+      },
+      {
+        garnishName: 'Cocktail Glass',
+        garnishImage: 'assets/images/icons/cocktail.png',
+      },
+      {
+        garnishName: 'Orange Wedge',
+        garnishImage: 'assets/images/icons/wedge.png',
+      },
+    ],
+    cocktailImage: 'assets/images/cocktails/hella-tasty.jpg',
+    cocktailIngredients: [
+      {
+        ingredientName: 'Vodka',
+        ingredientAmount: '1.5 oz'
+      },
+      {
+        ingredientName: 'Cranberry Juice',
+        ingredientAmount: '2 oz'
+      },
+      {
+        ingredientName: 'Other Stuff',
+        ingredientAmount: '0.5 oz'
+      },
+    ],
+    cocktailSteps: [
+      'Grab a shaker.',
+      'Fill shaker with ice.',
+      'Insert shaker into bartender.',
+      'Pour into old fashion glass.',
+      'Garnish with an orange wedge.'
+    ]
+  },
+  {
+    cocktailName: 'Fancy Name 2',
+    cocktailDescription: 'Longer Text.  Leverage agile frame works to provide a robust synopsis for high level overviews. Leverage agile frame works to provide.',
+    cocktailGarnishes: [
+      {
+        garnishName: 'Cocktail Glass',
+        garnishImage: 'assets/images/icons/cocktail.png',
+      },
+      {
+        garnishName: 'Orange Wedge',
+        garnishImage: 'assets/images/icons/wedge.png',
+      },
+      {
+        garnishName: 'Ice Cubes',
+        garnishImage: 'assets/images/icons/ice.png',
+      },
+    ],
+    cocktailImage: 'assets/images/cocktails/chocolatini.jpg',
+    cocktailIngredients: [
+      {
+        ingredientName: 'Vodka',
+        ingredientAmount: '1.5 oz'
+      },
+      {
+        ingredientName: 'Cranberry Juice',
+        ingredientAmount: '2 oz'
+      },
+      {
+        ingredientName: 'Other Stuff',
+        ingredientAmount: '0.5 oz'
+      },
+      {
+        ingredientName: 'Bitters',
+        ingredientAmount: '2 oz'
+      },
+      {
+        ingredientName: 'More Stuff',
+        ingredientAmount: '1.5 oz'
+      },
+    ],
+    cocktailSteps: [
+      'Grab a shaker.',
+      'Fill shaker with ice.',
+      'Insert shaker into bartender.',
+      'Pour into old fashion glass.',
+      'Garnish with an orange wedge.'
+    ]
+  },
+  {
+    cocktailName: 'Fancy Name with More Text',
+    cocktailDescription: 'Leverage agile frame works to provide a robust synopsis for high level overviews.',
+    cocktailImage: 'assets/images/cocktails/hella-tasty.jpg'
+  },
+  {
+    cocktailName: 'Fancy Name 4',
+    cocktailDescription: 'Leverage agile frame works to provide a robust synopsis for high level overviews.',
+    cocktailImage: 'assets/images/cocktails/hella-tasty.jpg'
+  },
+  {
+    cocktailName: 'Fancy Name 5',
+    cocktailDescription: 'Leverage agile frame works to provide a robust synopsis for high level overviews.',
+    cocktailImage: 'assets/images/cocktails/hella-tasty.jpg'
+  }
+]
+
+export default cocktails;
---
+++ client/src/utils/mixers.js
@@ -0,0 +1,64 @@
+const mixers = [
+  {
+    mixerCategory: 'Vodka',
+    mixerName: 'Absolut Vodka',
+    mixerImage: 'assets/images/mixers/nixabsolut.jpg'
+  },
+  {
+    mixerCategory: 'Vanilla Vodka',
+    mixerName: 'Stoli Vanil Vodka',
+    mixerImage: 'assets/images/mixers/nixstoli-vanil.jpg'
+  },
+  {
+    mixerCategory: 'Cranberry Juice',
+    mixerName: 'Cranberry Juice',
+    mixerImage: 'assets/images/mixers/nixcranberry-juice.jpg'
+  },
+  {
+    mixerCategory: 'Tequila',
+    mixerName: 'Don Julio Blanco Tequila',
+    mixerImage: 'assets/images/mixers/nixdon-julio-blanco-tequila.jpg'
+  },
+  {
+    mixerCategory: 'Orange Juice',
+    mixerName: 'Orange Juice',
+    mixerImage: 'assets/images/mixers/nixorange-juice.jpg'
+  },
+  {
+    mixerCategory: 'Gin',
+    mixerName: 'Bombay Sapphire Gin',
+    mixerImage: 'assets/images/mixers/nixabsolut.jpg'
+  },
+  {
+    mixerCategory: 'Tonic',
+    mixerName: 'Tonic',
+    mixerImage: 'assets/images/mixers/nixtonic.jpg'
+  },
+  {
+    mixerCategory: 'Dark Rum',
+    mixerName: 'Captain Morgan',
+    mixerImage: 'assets/images/mixers/nixcaptain-morgan.jpg'
+  },
+  {
+    mixerCategory: 'Cola',
+    mixerName: 'Coca Cola',
+    mixerImage: 'assets/images/mixers/nixcoca-cola.jpg'
+  },
+  {
+    mixerCategory: 'Rum',
+    mixerName: 'Bacardi',
+    mixerImage: 'assets/images/mixers/nixbacardi.jpg'
+  },
+  {
+    mixerCategory: 'Spiced Pear Liqueur',
+    mixerName: 'St. George Spiced Pear Liqueur',
+    mixerImage: 'assets/images/mixers/nixst-george-spiced-pear-liqueur.jpg'
+  },
+  {
+    mixerCategory: 'Chocolate Liqueur',
+    mixerName: 'Dorda Chocolate Liqueur',
+    mixerImage: 'assets/images/mixers/nixdorda-chocolate-liqueur.jpg'
+  },
+];
+
+export default mixers;
