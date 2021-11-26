dd375111cbbb079db66c62970249d99f1389c6d8
hifromkevin <lam.kevin86@gmail.com>
2021-11-26 12:25:14 -0800

    Cleans up homepage cocktail image

--- client/src/components/HomepageCocktails.js
+++ client/src/components/HomepageCocktails.js
@@ -8,24 +8,30 @@ const HomepageCocktails = ({ cocktails, setSelectedPage }) => (
     <Heading coloring='white' heading='Available Cocktails' />
     <div className='cocktailStation__container'>
       <div className='cocktailStation__list' style={{ width: (cocktails.length * 410) }}>
-        {cocktails.length && cocktails.map((cocktail, cocktailIndex) => (
-          <div
-            className='cocktail'
-            key={cocktailIndex}
-            onClick={() => setSelectedPage('selectedCocktail', cocktail)}
-          >
-            <div className='cocktail__container'>
-              <div className='cocktail__container__image'>
-                <img src={cocktail.cocktailImage} />
-              </div>
-              <div className='cocktail__container__tag'>
-                <span className='cocktail__container__tag__name'>{cocktail.cocktailName}</span>
-                <div className='cocktail__container__tag__separator'></div>
-                <span className='cocktail__container__tag__description'>{cocktail.cocktailDescription}</span>
+        {cocktails.length && cocktails.map((cocktail, cocktailIndex) => {
+          const heroImage = {
+            'backgroundImage': `url(${cocktail.cocktailImage})`,
+            'backgroundPosition': 'center',
+            'backgroundSize': 'cover'
+          }
+
+          return (
+            <div
+              className='cocktail'
+              key={cocktailIndex}
+              onClick={() => setSelectedPage('selectedCocktail', cocktail)}
+            >
+              <div className='cocktail__container'>
+                <div className='cocktail__container__image' style={heroImage}></div>
+                <div className='cocktail__container__tag'>
+                  <span className='cocktail__container__tag__name'>{cocktail.cocktailName}</span>
+                  <div className='cocktail__container__tag__separator'></div>
+                  <span className='cocktail__container__tag__description'>{cocktail.cocktailDescription}</span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
       {!cocktails.length && <p className="cocktailStation__unavailable">Add mixers to create a list of cocktails</p>}
     </div>
