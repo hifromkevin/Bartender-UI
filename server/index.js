0e242714aad8990c610a522eda00c56cefe5206e
hifromkevin <lam.kevin86@gmail.com>
2022-01-18 23:51:46 -0800

    Attempts to address Python by adding server directory

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.post('/makeDrink', (req, res) => {
   }
 
   const togglePin = (pinNumber, timer) => {
-    const args = ['runRelay.py', pinNumber, timer];
+    const args = ['./server/runRelay.py', pinNumber, timer];
     const gpioFunction = spawn('python3', args);
 
     gpioFunction.stdout.on('data', data => console.log(data.toString()));
