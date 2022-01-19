21c57ab8d4b858dedff61201867c94c5288ded47
hifromkevin <lam.kevin86@gmail.com>
2022-01-18 18:35:00 -0800

    Testing using Python to run GPIO

--- server/pyCleanup.py
+++ server/pyCleanup.py
@@ -1,17 +1,35 @@
 import RPi.GPIO as GPIO
 from threading import Timer
 
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 
-pins = [26, 19]
+# pins = [26, 19]
 
-for i in pins:
-    GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, GPIO.HIGH)
-    print('PIN! ' + str(i) )
+# for i in pins:
+#     GPIO.setup(i, GPIO.OUT)
+#     GPIO.output(i, GPIO.HIGH)
+#     print('PIN! ' + str(i) )
 
-def cleanUp():
-  GPIO.cleanup()
+# def cleanUp():
+#   GPIO.cleanup()
 
-t = Timer(2.0, cleanUp)
-t.start()
+# t = Timer(2.0, cleanUp)
+# t.start()
+
+GPIO.cleanup(26)
+
+
+
+
+
+# def turnOff(pins):
+#   for i in pins:
+#     cleanUp(i)
+
+# t = Timer(2.0, cleanUp, args=(26))
+# t.start()
+# t2 = Timer(4.0, cleanUp, args=(19))
+# t2.start()
+
+# t2 = Timer(2.0, GPIO.cleanup())
+# t2.start()
