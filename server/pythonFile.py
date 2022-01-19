1c661ef7b4a34659f4f37679efb62f6d479ea8a1
hifromkevin <lam.kevin86@gmail.com>
2022-01-18 22:06:19 -0800

    Tests running Python from JS

--- server/pythonFile.py
+++ server/pythonFile.py
@@ -4,18 +4,18 @@ import sys
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-print('ARG!', str(sys.argv))
-print('ARG!A', float(sys.argv[1]))
+print('Himom!', str(sys.argv))
+print('Himom2', int(sys.argv[1]))
 
 def turnOnPin(pin):
   GPIO.output(pin, 0)
   print('PIN ON: ' + str(pin))
 
 def turnOff():
-  GPIO.output(float(sys.argv[1]), 1)
+  GPIO.output(int(sys.argv[1]), 1)
 
-GPIO.setup(float(sys.argv[1]), GPIO.OUT, initial=1)
-turnOnPin(float(sys.argv[1]))
+GPIO.setup(int(sys.argv[1]), GPIO.OUT, initial=1)
+turnOnPin(int(sys.argv[1]))
 
 t = Timer(float(sys.argv[2]), turnOff)
 t.start()
