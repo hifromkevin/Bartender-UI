f71637b43c028d4935341c8dc2ae57cb4b4c5e30
hifromkevin <lam.kevin86@gmail.com>
2022-01-18 23:41:36 -0800

    Tweaking JS to Python call

--- server/jsToPy.js
+++ server/jsToPy.js
@@ -14,7 +14,7 @@ let timeframe = 0;
 
 const togglePin = (pinNumber, timer) => {
 
-  const args = ['runRelay.py', pinNumber, timer];
+  const args = ['./runRelay.py', pinNumber, timer];
   const gpioFunction = spawn('python', args);
 
   let returnOnOff;
