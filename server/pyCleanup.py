37eaa46364c12c72a1c6f133d97219ccdb7cdc6a
hifromkevin <lam.kevin86@gmail.com>
2022-01-18 18:22:33 -0800

    Testing using Python to run GPIO

--- server/pyCleanup.py
+++ server/pyCleanup.py
@@ -9,9 +9,9 @@ for i in pinsSelected:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, GPIO.HIGH)
 
-def cleanUp():
-  GPIO.cleanup()
-cleanUp()
+# def cleanUp():
+#   GPIO.cleanup()
+# cleanUp()
 
 # def turnOff(pins):
 #   for i in pins:
