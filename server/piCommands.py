a968318719646e515b564945bcfc84649689c38b
hifromkevin <lam.kevin86@gmail.com>
2022-01-18 19:56:52 -0800

    Testing using Python to run GPIO

--- server/piCommands.py
+++ server/piCommands.py
@@ -3,11 +3,14 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 def turnOnPin(pin):
-  GPIO.output(pin, True)
+  GPIO.setup(pin, GPIO.OUT, initial=1)
+  GPIO.output(pin, 0)
+  # GPIO.output(pin, True)
   print('PIN ON: ' + pin)
 
 def turnOffPin(pin):
-  GPIO.output(pin, False)
+  # GPIO.output(pin, False)
+  GPIO.output(pin, 1)
   print('PIN OFF: ' + pin)
 
 def cleanUp():
