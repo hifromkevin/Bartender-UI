1e928b303813cba52a9c10f203c65aa2e960f039
hifromkevin <lam.kevin86@gmail.com>
2022-01-18 23:22:48 -0800

    Tweaking JS to Python call

--- server/jsToPy.js
+++ server/jsToPy.js
@@ -22,12 +22,8 @@ const togglePin = (pinNumber, timer) => {
     returnOnOff = data.toString();
   });
 
-  process.stderr.on('data', function (data) {
-    returnOnOff += data.toString();
-  });
-
   gpioFunction.on('close', function (code) {
-    console.log(`${returnOnOff} - ${code}`)
+    console.log(code);
     return true;
   });
 }
