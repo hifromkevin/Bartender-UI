1fe362f481c85c2732cc67afa860d6d398fa8977
hifromkevin <lam.kevin86@gmail.com>
2022-01-18 21:34:37 -0800

    Tests running Python from JS

--- server/cmdPythonFile.py
+++ server/cmdPythonFile.py
@@ -9,9 +9,9 @@ def turnOnPin(pin):
   GPIO.output(pin, 0)
   print('PIN ON: ' + str(pin))
 
-def turnOff():
+def turnOff(pin):
   GPIO.setmode(GPIO.BCM)
-  GPIO.output(19, 1)
+  GPIO.output(pin, 1)
 
 # GPIO.setup(19, GPIO.OUT, initial=1)
 # turnOnPin(19)
@@ -19,7 +19,7 @@ def turnOff():
 # t = Timer(1.5, turnOff)
 # t.start()
 
-valz = sys.argv[1]
+valz = [[19,1.5],[26,3]]
 
 print('SYS! ' + sys.argv)
 
@@ -27,5 +27,5 @@ for i in valz:
   GPIO.setup(i[0], GPIO.OUT, initial=1)
   turnOnPin(i[0])
 
-  t = Timer(i[1], turnOff)
+  t = Timer(i[1], turnOff(i[0]))
   t.start()
